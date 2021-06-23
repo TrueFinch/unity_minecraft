98b22ffe1e0b7ee5c3372a7b9736fd28f15393ee
Vladimir Glushkov <vladimir.glushkov@mytona.com>
2021-06-23 22:29:02 +1000

    Create player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 6;
+    public float jumpSpeed = 8;
+    public float gravity = 20;
+    Vector3 moveDir = Vector3.zero;
+
+    float yRotation = 0;
+    float yRot;
+    float xRot;
+    public float sensitivity = 4;
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        MouseLook();
+
+    }
+
+    void MovePlayer()
+    {
+        bool jumped = false;
+        var character = GetComponent<CharacterController>();
+        if (character.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= moveSpeed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                jumped = true;
+                moveDir.y = jumpSpeed;
+            }
+        }
+        if (!jumped)
+        {
+            moveDir.y -= gravity * Time.deltaTime;
+        }
+        character.Move(moveDir * Time.deltaTime);
+    }
+
+    void MouseLook()
+    {
+        yRot = -Input.GetAxis("Mouse Y") * sensitivity;
+        xRot = Input.GetAxis("Mouse X") * sensitivity;
+        yRotation += yRot;
+        yRotation = Mathf.Clamp(yRotation, -80, 80);
+
+        if (xRot != 0)
+        {
+            transform.eulerAngles += new Vector3(0, xRot, 0);
+        }
+        if (yRot != 0)
+        {
+            cam.transform.eulerAngles = new Vector3(yRotation, transform.eulerAngles.y, 0);
+        }
+    }
+}
